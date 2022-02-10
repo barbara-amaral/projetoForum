4e7eaf15bfcbd1213c259327785a8c8d6abb1595
Bárbara Souza Amaral <barbara.amaral@ibm.com>
2022-02-10 12:20:19 -0300

    Envio de email de recomendacoes

---
+++ src/main/java/com/projetoforum/forum/config/RecomendacoesService.java
@@ -0,0 +1,52 @@
+package com.projetoforum.forum.config;
+
+import com.projetoforum.forum.config.security.TokenService;
+import com.projetoforum.forum.controller.UsuarioController;
+import com.projetoforum.forum.model.Topico;
+import com.projetoforum.forum.model.Usuario;
+import com.projetoforum.forum.service.TopicoService;
+import com.projetoforum.forum.service.UsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class RecomendacoesService {
+
+    @Autowired
+    TopicoService topicoService;
+
+    private static final Logger log = LoggerFactory.getLogger(UsuarioController.class);
+
+    public List<Topico> sugerirTopicos(Usuario usuario) {
+
+        List<Topico> topicos = new ArrayList<>();
+        List<Topico> lista = new ArrayList<>();
+
+        log.info("Procurando tags dos topicos do usuario "+ usuario.getEmail());
+
+        Set<String> tags = topicoService.findTopicoByAutorNome(usuario.getNome()).stream().map(Topico::getTag).collect(Collectors.toSet());
+
+        log.info("Procurando topicos baseados nas tags...");
+
+        for (String tag : tags) {
+            lista = topicoService.findTopicosByTag(tag);
+        }
+
+        for (Topico t: lista) {
+            if(!(t.getAutor().getNome()).equals((usuario.getNome()))){
+                topicos.add(t);
+            }
+        }
+
+        log.info("Retornando topicos.");
+
+        return topicos;
+    }
+}
