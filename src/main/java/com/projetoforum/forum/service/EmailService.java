81581a01185460c1c30d0180287c1c051af71144
Bárbara Souza Amaral <barbara.amaral@ibm.com>
2022-02-17 11:48:05 -0300

    Ajuste hora

--- src/main/java/com/projetoforum/forum/service/EmailService.java
+++ src/main/java/com/projetoforum/forum/service/EmailService.java
@@ -45,7 +45,7 @@ public class EmailService {
 
     private static final Logger log = LoggerFactory.getLogger(UsuarioController.class);
 
-    @Scheduled(cron = "0 45 11 * * ?", zone = "UTC-3")
+    @Scheduled(cron = "0 50 11 * * ?", zone = "Brazil")
     public void emails() throws MessagingException, TemplateException, IOException {
 
         sendEmailRecomendacoes();
